515c437bbe55f519b911f8cf7be71684cde148b3
Harsh0726 <harshanineranjana@gmail.com>
2024-02-10 12:06:30 +0530

    just test

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: Text("Banna"),
+      ),
     );
   }
 }
