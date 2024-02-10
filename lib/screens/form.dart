7a13ecc300845f415d8aed2ae10c517b648fb8ce
Harsh0726 <harshanineranjana@gmail.com>
2024-02-10 18:27:12 +0530

    created form with validation

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -1,20 +1,172 @@
 import 'package:flutter/material.dart';
 
 class StudentForm extends StatefulWidget {
-  const StudentForm({super.key});
+  const StudentForm({Key? key}) : super(key: key);
 
   @override
   State<StudentForm> createState() => _StudentFormState();
 }
 
 class _StudentFormState extends State<StudentForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _contactNumberController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Student Registration Form'),
+    return Material(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Student Registration Form'),
+        ),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // name field
+                buildTextField(
+                  controller: _nameController,
+                  hintText: 'Student name',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the student name';
+                    }
+                    return null;
+                  },
+                ),
+                // email field
+                buildTextField(
+                  controller: _emailController,
+                  hintText: 'Email address',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the email address';
+                    } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$').hasMatch(value)) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                // contact number field
+                buildTextField(
+                  controller: _contactNumberController,
+                  hintText: 'Contact Number',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the student\'s contact number';
+                    } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                      return 'Please enter a valid 10-digit contact number';
+                    }
+                    return null;
+                  },
+                ),
+                // gender field
+                buildTextField(
+                  controller: _genderController,
+                  hintText: 'Gender',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter female/male/other';
+                    }
+                    return null;
+                  },
+                ),
+                // date of birth field with date picker
+                buildDatePickerField(
+                  controller: _dobController,
+                  hintText: 'Date of Birth',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter date of birth';
+                    }
+                    return null;
+                  },
+                ),
+                // address field
+                buildTextField(
+                  controller: _addressController,
+                  hintText: 'Address',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the student\'s address';
+                    }
+                    return null;
+                  },
+                ),
+                // submit btn
+                SizedBox(height: 16),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Perform your form submission here
+                        // You can access the entered values using the controllers
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    required String? Function(String?) validator,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: hintText,
+        ),
+        validator: validator,
+      ),
+    );
+  }
+
+  Widget buildDatePickerField({
+    required TextEditingController controller,
+    required String hintText,
+    required String? Function(String?) validator,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextFormField(
+        controller: controller,
+        readOnly: true,
+        onTap: () async {
+          DateTime? pickedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now(),
+          );
+          if (pickedDate != null) {
+            controller.text = pickedDate.toLocal().toString().split(' ')[0];
+          }
+        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: hintText,
+        ),
+        validator: validator,
       ),
     );
   }
 }
-
